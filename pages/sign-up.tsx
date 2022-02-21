46dd186254ba74058853a91adfe67f0aa2e8b8e0
jiwlee <ddazi9576@google.com>
2022-02-21 20:55:35 +0900

    style: 기본 프로필 이미지 설정 & SignUpPage 레이아웃 설정

--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { useCallback, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import useInput from "../hooks/useInput";
@@ -9,7 +9,10 @@ const SignUpPage: NextPage = () => {
   const [email, onChangeEmail] = useInput("");
   const [password, onChangePassword] = useInput("");
   const [nickname, onChangeNickname] = useInput("");
-  const [previewImageDataURL, setPreviewImageDataURL] = useState<string>("");
+  const [previewImageDataURL, setPreviewImageDataURL] = useState<string>(
+    "/default-profile.png"
+  );
+  const uploadFileRef = useRef<HTMLInputElement | null>(null);
 
   const onUploadFile = useCallback((event) => {
     const {
@@ -24,51 +27,73 @@ const SignUpPage: NextPage = () => {
     reader.readAsDataURL(theFile);
   }, []);
 
+  const onClickChangeProfileImage = useCallback(() => {
+    uploadFileRef.current?.click();
+  }, []);
+
   const onSubmit = useCallback((event) => {
     event.preventDefault();
     router.push("/");
   }, []);
 
   return (
-    <main>
-      <h1>회원가입</h1>
-      <form onSubmit={onSubmit}>
+    <main className="flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold mb-10">회원가입</h1>
+      <form className="flex flex-col" onSubmit={onSubmit}>
+        <div className="mb-2 text-center">
+          <Image
+            className="rounded-full"
+            src={previewImageDataURL}
+            alt="profile image preview"
+            width={200}
+            height={200}
+            objectPosition={`50% 50%`}
+            objectFit="cover"
+          />
+        </div>
+        <button
+          className="mb-4 block"
+          type="button"
+          onClick={onClickChangeProfileImage}
+        >
+          프로필이미지변경
+        </button>
         <input
-          className="px-2 py-1 border rounded"
-          type="text"
+          className="mb-4 w-80 hidden"
+          type="file"
+          onChange={onUploadFile}
+          ref={uploadFileRef}
+        />
+        <input
+          className="px-3 py-2 border rounded mb-4 w-80"
+          type="email"
           required
           placeholder="EMAIL"
           value={email}
           onChange={onChangeEmail}
         />
         <input
-          className="px-2 py-1 border rounded"
-          type="text"
+          className="px-3 py-2 border rounded mb-4 w-80"
+          type="password"
           required
           placeholder="PASSWORD"
           value={password}
           onChange={onChangePassword}
         />
         <input
-          className="px-2 py-1 border rounded"
+          className="px-3 py-2 border rounded mb-4 w-80"
           type="text"
           required
           placeholder="NICKNAME"
           value={nickname}
           onChange={onChangeNickname}
         />
-        {previewImageDataURL && (
-          <div className="relative w-52 h-52">
-            <Image
-              src={previewImageDataURL}
-              alt="profile image preview"
-              layout="fill"
-              className="rounded-full"
-            />
-          </div>
-        )}
-        <input type="file" onChange={onUploadFile} />
-        <button type="submit">회원가입하기</button>
+        <button className="bg-green-400 py-2 rounded mb-2 w-80" type="submit">
+          회원가입하기
+        </button>
+        <button className="border py-2 rounded w-80" type="button">
+          로그인하러가기
+        </button>
       </form>
     </main>
   );
