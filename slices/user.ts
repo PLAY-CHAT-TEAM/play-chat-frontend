6520c798506b1cf6ddd8e07160dcf6c05e28b329
jiwlee <ddazi9576@google.com>
2022-02-24 18:51:25 +0900

    feat: store,reducer,slice 생성

---
+++ slices/user.ts
@@ -0,0 +1,38 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const initialState = {
+  email: "",
+  nickname: "",
+  profileImage: "",
+  accessToken: "",
+  refreshToken: "",
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser(state, action) {
+      state.email = action.payload.email;
+      state.nickname = action.payload.nickname;
+      state.profileImage = action.payload.profileImage;
+      state.accessToken = action.payload.accessToken;
+      state.refreshToken = action.payload.refreshToken;
+    },
+    setNickname(state, action: PayloadAction<string>) {
+      state.nickname = action.payload;
+    },
+    setProfileImage(state, action: PayloadAction<string>) {
+      state.profileImage = action.payload;
+    },
+    setAccessToken(state, action: PayloadAction<string>) {
+      state.accessToken = action.payload;
+    },
+    setRefreshToken(state, action: PayloadAction<string>) {
+      state.refreshToken = action.payload;
+    },
+  },
+  extraReducers: (builder) => {},
+});
+
+export default userSlice;
