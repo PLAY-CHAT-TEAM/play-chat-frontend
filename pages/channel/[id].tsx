96536b8e3505cf56815126667ce54dfe1b4994b5
jiwlee <ddazi9576@google.com>
2022-02-28 12:48:20 +0900

    feat: getStaticPaths, getStaticProps

--- pages/channel/[id].tsx
+++ pages/channel/[id].tsx
@@ -1,6 +1,45 @@
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
 import Link from "next/link";
 
-const ChannelPage = () => {
+interface Channel {
+  id: string;
+  name: string;
+}
+
+const channelArray: Channel[] = [
+  { id: "1", name: "general" },
+  { id: "2", name: "random" },
+  { id: "3", name: "study" },
+];
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = channelArray.map((channel) => {
+    const { id } = channel;
+    return {
+      params: { id },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const channel: Channel | undefined = channelArray.find(
+    (v) => v.id === params?.id
+  );
+  return {
+    props: {
+      channel,
+    },
+    revalidate: 10,
+  };
+};
+
+const ChannelPage = ({
+  channel,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <div className="flex m-2">
       <div>
@@ -29,13 +68,13 @@ const ChannelPage = () => {
           <span>CHANNELS</span>
           <ul>
             <li>
-              <Link href={`channel/${1}`}>General</Link>
+              <Link href={`/channel/${1}`}>General</Link>
             </li>
             <li>
-              <Link href={`channel/${2}`}>Random</Link>
+              <Link href={`/channel/${2}`}>Random</Link>
             </li>
             <li>
-              <Link href={`channel/${3}`}>Study</Link>
+              <Link href={`/channel/${3}`}>Study</Link>
             </li>
           </ul>
         </div>
@@ -43,17 +82,17 @@ const ChannelPage = () => {
           <span>MESSAGES</span>
           <ul>
             <li>
-              <Link href={`dm/${1}`}>Kycho</Link>
+              <Link href={`/dm/${1}`}>Kycho</Link>
             </li>
             <li>
-              <Link href={`channel/${2}`}>Jiwlee</Link>
+              <Link href={`/channel/${2}`}>Jiwlee</Link>
             </li>
           </ul>
         </div>
       </div>
       <div>
         <div>
-          <h1>General</h1>
+          <h1>{channel.name}</h1>
           <button>Channel details</button>
           <div>
             <ul>
