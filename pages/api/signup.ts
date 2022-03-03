2b0a15984f8d3fbc830971b827435961f82513b2
jiwlee <53519832+jiwlee97@users.noreply.github.com>
2022-03-03 14:19:44 +0900

    feat: 회원가입 기능 구현 (#26)
    * feat: npm install axios
    
    * feat: custom ApiError class 생성 & axiosGet, axiosPost 함수 생성
    
    * feat: nextjs API Routes signup 생성
    
    * feat: SignUpPage에서 회원가입 api 구현
    
    * remove: lib/api.ts 파일 삭제
    
    * feat: 서버와 api연동 성공
    
    * feat: react-toastify 적용
    
    * feat: input maxLength 추가
    
    * feat: profileImage File formData에 append
    
    Co-authored-by: jiwlee <ddazi9576@google.com>

---
+++ pages/api/signup.ts
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import FormData from "form-data";
+import axios, { AxiosError } from "axios";
+
+const handleSignUp = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { BACK_URL } = process.env;
+
+  if (req.method !== "POST") {
+    res.status(405).end();
+    return;
+  }
+  const { email, password, nickname, profileImage } = req.body;
+  const formData = new FormData();
+  formData.append("email", email);
+  formData.append("password", password);
+  formData.append("nickname", nickname);
+  formData.append("profileImage", profileImage);
+  const formHeaders = formData.getHeaders();
+
+  try {
+    const response = await axios.post(
+      `${BACK_URL}/api/members/sign-up`,
+      formData,
+      {
+        headers: { ...formHeaders },
+      }
+    );
+    res.status(response.status).json({ ...response.data });
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    res
+      .status(axiosError.response?.status || 500)
+      .json({ ...axiosError.response?.data });
+  }
+};
+
+export default handleSignUp;
--- pages/sign-up.tsx
+++ pages/sign-up.tsx
@@ -6,6 +6,8 @@ import useInput from "@/hooks/useInput";
 import SignPage from "@/layouts/SignPage";
 import Input from "@/components/Input";
 import Error from "@/components/Error";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const SignUpPage: NextPageWithLayout = () => {
   const router = useRouter();
@@ -13,10 +15,10 @@ const SignUpPage: NextPageWithLayout = () => {
   const [password, onChangePassword] = useInput("");
   const [passwordCheck, onChangePasswordCheck] = useInput("");
   const [nickname, onChangeNickname] = useInput("");
-  const [previewImageDataURL, setPreviewImageDataURL] = useState<string>(
-    "/default-profile.png"
-  );
+  const [profileImage, setProfileImage] = useState<File>();
+  const [previewImage, setPreviewImage] = useState("/default-profile.png");
   const uploadFileRef = useRef<HTMLInputElement | null>(null);
+  const [loading, setLoading] = useState(false);
   const [passwordCheckError, setPasswordCheckError] = useState("");
   const [passwordRegError, setPasswordRegError] = useState("");
   const passwordRegExp =
@@ -27,10 +29,11 @@ const SignUpPage: NextPageWithLayout = () => {
       target: { files },
     } = event;
     const theFile = files[0];
+    setProfileImage(theFile);
     const reader = new FileReader();
 
     reader.onloadend = () => {
-      setPreviewImageDataURL(reader.result as string);
+      setPreviewImage(reader.result as string);
     };
     reader.readAsDataURL(theFile);
   }, []);
@@ -40,8 +43,9 @@ const SignUpPage: NextPageWithLayout = () => {
   }, []);
 
   const onSubmit = useCallback(
-    (event) => {
+    async (event) => {
       event.preventDefault();
+      setLoading(true);
       if (
         !email.trim() ||
         !password.trim() ||
@@ -52,7 +56,27 @@ const SignUpPage: NextPageWithLayout = () => {
       ) {
         return;
       }
-      router.push("/");
+      axios
+        .post("/api/signup", { email, password, profileImage, nickname })
+        .then((response) => {
+          const { nickname, email } = response.data;
+          toast(`${nickname}님 회원가입을 축하합니다!`);
+          router.push("/sign-in");
+        })
+        .catch((error) => {
+          toast.error(error.response.data.message, {
+            position: toast.POSITION.TOP_RIGHT,
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+        })
+        .finally(() => {
+          setLoading(false);
+        });
     },
     [router, email, password, passwordCheck, nickname, passwordCheckError]
   );
@@ -77,82 +101,93 @@ const SignUpPage: NextPageWithLayout = () => {
     }
   }, [password]);
 
+  console.log("profileImage", profileImage);
+
+  if (loading) {
+    return <div>로딩중...</div>;
+  }
+
   return (
-    <form className="flex flex-col" onSubmit={onSubmit}>
-      <div className="mb-2 text-center">
-        <Image
-          className="rounded-full"
-          src={previewImageDataURL}
-          alt="profile image preview"
-          width={200}
-          height={200}
-          objectPosition={`50% 50%`}
-          objectFit="cover"
+    <>
+      <form className="flex flex-col" onSubmit={onSubmit}>
+        <div className="mb-2 text-center">
+          <Image
+            className="rounded-full"
+            src={previewImage}
+            alt="profile image preview"
+            width={200}
+            height={200}
+            objectPosition={`50% 50%`}
+            objectFit="cover"
+          />
+        </div>
+        <button
+          className="mb-5 text-sm"
+          type="button"
+          onClick={onClickChangeProfileImage}
+        >
+          프로필이미지변경
+        </button>
+        <input
+          className="hidden"
+          type="file"
+          onChange={onUploadFile}
+          ref={uploadFileRef}
         />
-      </div>
-      <button
-        className="mb-5 text-sm"
-        type="button"
-        onClick={onClickChangeProfileImage}
-      >
-        프로필이미지변경
-      </button>
-      <input
-        className="hidden"
-        type="file"
-        onChange={onUploadFile}
-        ref={uploadFileRef}
-      />
-      <Input
-        type="email"
-        required
-        placeholder="이메일"
-        value={email}
-        onChange={onChangeEmail}
-      />
-      <input
-        className={`px-3 py-2 border rounded w-80 focus:outline-sky-700 ${
-          !passwordRegError && "mb-4"
-        }`}
-        type="password"
-        required
-        placeholder="비밀번호"
-        value={password}
-        onChange={onChangePassword}
-      />
-      {passwordRegError && <Error message={passwordRegError} />}
-      <input
-        className={`px-3 py-2 border rounded w-80 focus:outline-sky-700 ${
-          !passwordCheckError && "mb-4"
-        }`}
-        type="password"
-        required
-        placeholder="비밀번호확인"
-        value={passwordCheck}
-        onChange={onChangePasswordCheck}
-      />
-      {passwordCheckError && <Error message={passwordCheckError} />}
-      <Input
-        type="text"
-        required
-        placeholder="닉네임"
-        value={nickname}
-        onChange={onChangeNickname}
-      />
-      <button
-        className="bg-sky-600 text-white py-2 rounded mb-2 w-80 focus:outline-sky-700"
-        type="submit"
-      >
-        회원가입하기
-      </button>
-      <button
-        className="border py-2 rounded w-80 text-sm focus:outline-sky-700"
-        type="button"
-        onClick={onClickSignIn}
-      >
-        로그인하러가기
-      </button>
-    </form>
+        <Input
+          type="email"
+          required
+          placeholder="이메일"
+          value={email}
+          onChange={onChangeEmail}
+          maxLength={50}
+        />
+        <input
+          className={`px-3 py-2 border rounded w-80 focus:outline-sky-700 ${
+            !passwordRegError && "mb-4"
+          }`}
+          type="password"
+          required
+          placeholder="비밀번호"
+          value={password}
+          onChange={onChangePassword}
+        />
+        {passwordRegError && <Error message={passwordRegError} />}
+        <input
+          className={`px-3 py-2 border rounded w-80 focus:outline-sky-700 ${
+            !passwordCheckError && "mb-4"
+          }`}
+          type="password"
+          required
+          placeholder="비밀번호확인"
+          value={passwordCheck}
+          onChange={onChangePasswordCheck}
+        />
+        {passwordCheckError && <Error message={passwordCheckError} />}
+        <Input
+          type="text"
+          required
+          placeholder="닉네임"
+          value={nickname}
+          onChange={onChangeNickname}
+          maxLength={50}
+        />
+        <button
+          className="bg-sky-600 text-white py-2 rounded mb-2 w-80 focus:outline-sky-700"
+          type="submit"
+          disabled={loading}
+        >
+          회원가입하기
+        </button>
+        <button
+          className="border py-2 rounded w-80 text-sm focus:outline-sky-700"
+          type="button"
+          onClick={onClickSignIn}
+        >
+          로그인하러가기
+        </button>
+      </form>
+    </>
   );
 };
 
