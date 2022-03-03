8210b5ce07e10b37dca02e5f1f06323e19106134
jiwlee <ddazi9576@google.com>
2022-03-03 22:38:57 +0900

    feat: 로그인 api nextjs api route에서 구현

--- pages/api/signin.ts
+++ pages/api/signin.ts
@@ -1,13 +1,32 @@
+import axios, { AxiosError } from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const handleSignIn = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({
-    email: "jiwlee@example.com",
-    nickname: "jiwlee",
-    profileImage: "",
-    accessToken: "accessToken",
-    refreshToken: "refreshToken",
-  });
+const handleSignIn = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { BACK_URL } = process.env;
+
+  if (req.method !== "POST") {
+    res.status(405).end();
+    return;
+  }
+  const { email, password } = req.body;
+  try {
+    const response = await axios.post(
+      `${BACK_URL}/api/auth/sign-in`,
+      { email, password },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    );
+    res.status(response.status).json({ ...response.data });
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    res
+      .status(axiosError.response?.status || 500)
+      .json({ ...axiosError.response?.data });
+  }
 };
 
 export default handleSignIn;
