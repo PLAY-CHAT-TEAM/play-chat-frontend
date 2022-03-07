bef7e75ca38fa6c7c7196ea2c0be26e9f811c4ee
jiwlee <ddazi9576@google.com>
2022-03-07 12:59:36 +0900

    feat: 임의데이터에서 redux의 user data를 사용

--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -1,18 +1,24 @@
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/reducer";
 
 const UserInfo = () => {
+  const user = useSelector((state: RootState) => state.user);
+
   return (
     <div className="flex justify-around items-center mb-4 bg-sky-700 rounded p-3 text-white">
-      <Image
-        className="rounded-full"
-        src="/default-profile.png"
-        width="50"
-        height="50"
-      />
+      {user.imageUrl && (
+        <Image
+          className="rounded-full"
+          src={user.imageUrl}
+          width="50"
+          height="50"
+        />
+      )}
       <div className="flex flex-col ml-4">
-        <span className="text-xl">Jiwlee</span>
+        <span className="text-xl">{user.nickname}</span>
         <span className="text-xs">Active</span>
       </div>
       <button>
