cf48f6c1c7d9b091fb0b8a38cb75c55e8bc81d0b
jiwlee <ddazi9576@google.com>
2022-03-07 13:14:44 +0900

    feat: axios.defaults.headers.common['Authorization'] 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { NextPage } from "next";
-import { ReactElement, ReactNode } from "react";
+import { ReactElement, ReactNode, useEffect } from "react";
 import { wrapper } from "@/store/index";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/reducer";
+import axios from "axios";
 
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -16,6 +19,16 @@ type AppPropsWithLayout = AppProps & {
 
 function WrappedApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page);
+  const user = useSelector((state: RootState) => state.user);
+
+  useEffect(() => {
+    if (user.accessToken) {
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${user.accessToken}`;
+      axios.defaults.headers.common["withCredentials"] = true;
+    }
+  }, [user]);
 
   return getLayout(
     <>
