f5e153748f3a1d8352255a2d53c8987e22954782
jiwlee <ddazi9576@google.com>
2022-03-10 00:41:57 +0900

    feat: /api/members/me 요청 실패 에러 처리

--- saga/userSaga.ts
+++ saga/userSaga.ts
@@ -1,16 +1,19 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { call, put, takeEvery } from "redux-saga/effects";
 import { setUser } from "@/slices/user";
+import { toast } from "react-toastify";
+import Router from "next/router";
 
 function* workGetUserInfo() {
   try {
     const response: AxiosResponse = yield call(() => {
       return axios.get("/api/members/me");
     });
-    yield alert(response.data);
     yield put(setUser(response.data));
   } catch (error) {
     const axiosError = error as AxiosError;
+    yield call(Router.push, "/sign-in");
+    yield call(toast, axiosError.response?.data.message);
   }
 }
 
