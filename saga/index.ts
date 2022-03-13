c25ae57010712b7e22b0c07b0704405c9c6fae7b
jiwlee <ddazi9576@google.com>
2022-03-14 01:44:44 +0900

    refactor: signinSaga 삭제

--- saga/index.ts
+++ saga/index.ts
@@ -1,7 +1,6 @@
 import { all, fork } from "redux-saga/effects";
-import { signinSaga } from "./signinSaga";
 import { userSaga } from "./userSaga";
 
 export default function* rootSaga() {
-  yield all([fork(signinSaga), fork(userSaga)]);
+  yield all([fork(userSaga)]);
 }
