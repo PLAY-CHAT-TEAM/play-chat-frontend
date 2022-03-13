0c64f5abf7c477ba385e10b154c1d7662b7d9392
jiwlee <53519832+jiwlee97@users.noreply.github.com>
2022-03-14 01:47:14 +0900

    refactor: remove signinSaga (#37)
    * refactor: signinSaga 삭제
    
    * refactor: 로그인 dispatch가 아닌 axios.post로 구현
    
    Co-authored-by: jiwlee <ddazi9576@google.com>

--- store/reducer.ts
+++ store/reducer.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import userSlice from "@/slices/user";
-import signinSlice from "@/slices/signin";
 
 const rootReducer = combineReducers({
   [userSlice.name]: userSlice.reducer,
-  [signinSlice.name]: signinSlice.reducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
